5c5601951c72236af9f1ce1a53fb6cf0bc024882
Kay Roepke <kroepke@googlemail.com>
2016-07-12 15:43:48 +0200

    return null if the user could not been found
    we'll make it configurable to auto-create users later

--- src/main/java/org/graylog/plugins/auth/httpheaders/HttpHeadersAuth.java
+++ src/main/java/org/graylog/plugins/auth/httpheaders/HttpHeadersAuth.java
@@ -6,19 +6,27 @@ import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAccount;
 import org.apache.shiro.authc.credential.AllowAllCredentialsMatcher;
 import org.apache.shiro.realm.AuthenticatingRealm;
+import org.graylog2.plugin.database.users.User;
 import org.graylog2.shared.security.HttpHeadersToken;
 import org.graylog2.shared.security.ShiroSecurityContext;
+import org.graylog2.shared.users.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
 import javax.ws.rs.core.MultivaluedMap;
 
 public class HttpHeadersAuth extends AuthenticatingRealm {
-    private static final Logger log = LoggerFactory.getLogger(HttpHeadersAuth.class);
+    private static final Logger LOG = LoggerFactory.getLogger(HttpHeadersAuth.class);
+    private static final String DEFAULT_HEADER = "Remote-User";
 
     public static final String NAME = "trusted-headers";
 
-    public HttpHeadersAuth() {
+    private final UserService userService;
+
+    @Inject
+    public HttpHeadersAuth(UserService userService) {
+        this.userService = userService;
         setAuthenticationTokenClass(HttpHeadersToken.class);
         setCredentialsMatcher(new AllowAllCredentialsMatcher());
         setCachingEnabled(false);
@@ -29,12 +37,18 @@ public class HttpHeadersAuth extends AuthenticatingRealm {
         HttpHeadersToken headersToken = (HttpHeadersToken) token;
         final MultivaluedMap<String, String> requestHeaders = headersToken.getHeaders();
 
-        if (requestHeaders.containsKey("remote-user")) {
-            final String userName = requestHeaders.getFirst("remote-user");
-            log.trace("Trusted header {} set, continuing with user name {}", "Remote-User", userName);
+        if (requestHeaders.containsKey(DEFAULT_HEADER.toLowerCase())) {
+            final String userName = requestHeaders.getFirst(DEFAULT_HEADER.toLowerCase());
+            final User user = userService.load(String.valueOf(userName));
+            if (user == null) {
+                LOG.trace("No user named {} found, not using content of trusted header {}", userName, DEFAULT_HEADER);
+                return null;
+            }
+
+            LOG.trace("Trusted header {} set, continuing with user name {}", DEFAULT_HEADER, user.getName());
 
             ShiroSecurityContext.requestSessionCreation(true);
-            return new SimpleAccount(userName, null, NAME);
+            return new SimpleAccount(user.getName(), null, NAME);
         }
         return null;
     }
